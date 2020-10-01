d8fd15b9933917c6bacb9cb89dd42a680debfd80
Silvarrn <44820874+Silvarrn@users.noreply.github.com>
2020-10-01 18:09:06 +0100

    Attempt to support Modern Authentication.

--- exclaimer-cloud/signatures-for-office-365/ReportToOriginatorEnabled-o365.ps1
+++ exclaimer-cloud/signatures-for-office-365/ReportToOriginatorEnabled-o365.ps1
@@ -12,19 +12,27 @@
 .NOTES
     Email: support@exclaimer.com
     Date: 13th May 2017
+    Updated: 1st October 2020
 .PRODUCTS
     Exclaimer Cloud - Signatures for Office 365
 .REQUIREMENTS
     - Global Administrator Account
 .VERSION
      1.0 - Set ReportToOriginatorEnabled to True
+     2.0 - Updated to Support Modern Authentication
 #>
  
-function o365_connect {
+<#function o365_connect {
     # below connects to Office 365
     $credential = Get-Credential
     $session = New-PSSession -ConfigurationName Microsoft.Exchange -Credential $credential -ConnectionUri https://ps.outlook.com/powershell -Authentication Basic -AllowRedirection
     Import-PSSession $session
+}#>
+
+function o365_connect {
+    Write-Host ("A prompt to login to Microsoft 365 will appear shortly. If any errors appear after this message, please provide a copy of these errors to Exclaimer Support")
+    Import-Module ExchangeOnlineManagement
+    Connect-ExchangeOnline -UserPrincipalName $upn -ShowProgress $true
 }
  
 function o365_gather {
@@ -40,12 +48,10 @@ function o365_gather {
         If ($dirsync -ne $null) {
             Write-Host ("Below are the Office 365 groups sync'd from AD with the value of False") -ForegroundColor Green
             Write-Output $dirsync | Select DisplayName,ReportToOriginatorEnabled | Format-Table
-            Write-Host ("The script will now end")
             Exit
         }
         Else {
             Write-Host ("There are no groups with ReportToOriginatorEnabled set to False") -ForegroundColor Green
-            Write-Host ("This script will now end") -ForegroundColor Green
             Exit
         }
     }
@@ -58,6 +64,9 @@ function o365_change {
         $groups | Set-DistributionGroup -ReportToOriginatorEnabled $true
         Write-Host ("Group changes complete!") -ForegroundColor White
     }
+     Write-Host ("Disconnecting from Microsoft 365.") -ForegroundColor Green
+    Disconnect-ExchangeOnline | echo a
+    Write-Host ("This script will now end") -ForegroundColor Green
 }
  
 o365_connect
