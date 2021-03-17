61e252505360b89cd18d02bb169a6e6d2f6437d7
Silvarrn <44820874+Silvarrn@users.noreply.github.com>
2021-03-17 17:03:46 +0000

    Update remove-owa-signatures.ps1

--- general/remove-owa-signatures.ps1
+++ general/remove-owa-signatures.ps1
@@ -16,10 +16,10 @@
 
 # Function to connect to Office 365 in current Window
 function o365_connect {
-    # below connects to Office 365
-    $credential = Get-Credential
-    $session = New-PSSession -ConfigurationName Microsoft.Exchange -Credential $credential -ConnectionUri https://ps.outlook.com/powershell -Authentication Basic -AllowRedirection
-    Import-PSSession $session
+    # below should connect to Office 365
+    Write-Host ("A prompt to login to Microsoft 365 will appear shortly. If any errors appear after this message, please provide a copy of these errors to Exclaimer Support")
+    Import-Module ExchangeOnlineManagement
+    Connect-ExchangeOnline -UserPrincipalName $upn -ShowProgress $true
 }
 
 # Function exchange connect
