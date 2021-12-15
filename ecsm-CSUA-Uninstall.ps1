c5b50132ee7af176df433b7012712204a3b65dab
NChaves <46609075+NChaves@users.noreply.github.com>
2021-12-15 11:53:24 +0000

    Update ecsm-CSUA-Uninstall.ps1

--- ecsm-CSUA-Uninstall.ps1
+++ ecsm-CSUA-Uninstall.ps1
@@ -1,3 +1,4 @@
+#
 <#
 .SYNOPSIS
     Script to uninstall all instances of the Cloud Signature Update Agent
@@ -15,31 +16,20 @@
 #>
 
 # Click Once uninstall
+Write-Output "Uninstalling Click Once Installs"
 $app = "Exclaimer Cloud Signature Update Agent"
 
 $InstalledApplicationNotMSI = Get-ChildItem HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall | foreach-object {Get-ItemProperty $_.PsPath}
-$UninstallString = $InstalledApplicationNotMSI | Where-Object { $_.displayname -match "$app" } | Select-Object UninstallString 
+$UninstallString = $InstalledApplicationNotMSI | ? { $_.displayname -match "$app" } | select UninstallString 
 
-if (!$UninstallString.UninstallString) {
-Write-Output "No ClickOnce agent found"
-}
-
-if ($UninstallString.UninstallString) {
 $wshell = new-object -com wscript.shell
 $selectedUninstallString = $UninstallString.UninstallString
 $wshell.run("cmd /c $selectedUninstallString")
 Start-Sleep 5
 $wshell.sendkeys("`"OK`"~")
-Write-Output "ClickOnce agent removed"
-}
 
 # MSI Uninstall
+Write-Output "Uninstalling MSI Installs"
 $MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Exclaimer Cloud Signature Update Agent"}
-if (!$MyApp) {
-Write-Output "No MSI installed agent found"
-}
-
-if ($MyApp) {
 $MyApp.Uninstall() | Out-Null
-Write-Output "MSI installed agent removed"
-}
+Write-Output "All Cloud Signature Update Agent Installations removed"
