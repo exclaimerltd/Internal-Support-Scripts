00dfaf7948a036ae8efa5a842e4fea4548dbbede
NChaves <nuno_mjc@hotmail.com>
2024-01-23 09:25:00 +0000

    improvements

--- MsGraphUsers.ps1
+++ MsGraphUsers.ps1
@@ -15,12 +15,10 @@
 # 
 # .REQUIREMENTS
 #     - Global Administrator access to the Microsoft Tenant
-#     - Requires path "C:\Temp"
 #     - Microsoft.Graph.Authentication - https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.authentication/?view=graph-powershell-1.0
 #     - Microsoft.Graph.Beta.Users - https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.beta.users/?view=graph-powershell-beta
 # 
-# .VERSION
-# 
+# .VERSION 
 # 
 # 	1.0.0
 # 
@@ -77,118 +75,103 @@ function connect-MicrosoftGraph {
 
 #Check "C:\Temp"
 function checkTemp {
-    if (Test-Path -Path $Path){
-    Write-Output ("Output Path exists") | Out-File $OutFile
-    }
-    Else {
-    New-Item $Path -ItemType Directory
-    }
+            if (Test-Path -Path $Path){
+            Write-Output ("Output Path exists") | Out-File $OutFile
+            }
+            Else {
+            New-Item $Path -ItemType Directory
+            }
 }
 
 function findBy {
+            try {
+                [string[]]$options = 'Search by: Display Name', 'Search by: UPN', 'Search by: email or proxy address' ,'Open Output folder' ,'Exit'
 
-    try {
-        [string[]]$options = 'Display Name', 'UPN', 'Email'
+                Write-Output "Please choose what to search for:`n"
 
-        Write-Output "Please choose what to search for:`n"
+                1..$options.Length | foreach-object { Write-Output "$($_): $($options[$_-1])" }
 
-        1..$options.Length | foreach-object { Write-Output "$($_): $($options[$_-1])" }
-
-        [ValidateScript({$_ -ge 1 -and $_ -le $options.Length})]
-        [int]$number = Read-Host "`nPress a number to choose how to search"
-            if($?) {
-                Write-Output "You chose: $number`n"
-            }
-    } catch {
-        Write-Host "Invalid entry"
-        $tryAgain = Read-Host ("Do you want to search again? Y/n")
-            if ($tryAgain -eq "y") {
+                [ValidateScript({$_ -ge 1 -and $_ -le $options.Length})]
+                [int]$number = Read-Host "`nPress a number to choose how to search"
+                    if($?) {
+                        Write-Output "You chose: $number`n"
+                    }
+            } catch {
+                Write-Host "Invalid entry"
                 findBy
-            }
-            Else {            
-                endSession
-            }
+                Break
 
-    } finally {
-        if ($number -eq 1) {
-            $searchPropriety="DisplayName"         
-            $searchPrompt = "Please enter the Display Name of the required user" 
-            find-users
-        }
-        if ($number -eq 2) {     
-            $searchPropriety="UserPrincipalName"   
-            $searchPrompt = "Please enter the full UPN of the required user" 
-            find-users
-        }
-        if ($number -eq 3) {
-            $searchPropriety="ProxyAddresses"
-            $searchPrompt = "Please enter an email address for the required user"
-            find-users
-        }
-    }
+            } finally {
+                if ($number -eq 1) {
+                $searchPropriety="DisplayName"         
+                $searchPrompt = "Please enter the Display Name of the required user" 
+                find-users                
+                } 
+                if ($number -eq 2) {     
+                $searchPropriety="UserPrincipalName"   
+                $searchPrompt = "Please enter the full UPN of the required user" 
+                find-users
+                } 
+                if ($number -eq 3) {
+                $searchPropriety="ProxyAddresses"
+                $searchPrompt = "Please enter an email or proxy address address for the required user"
+                find-users
+                } 
+                if ($number -eq 4) {
+                openOutPath       
+                findBy
+                } 
+                if ($number -eq 5) {
+                endSession
+                }
+            }
+    Break        
 }
 
-
 function find-users{
-    $DateTimeRun = (Get-Date -Format "dddd MM/dd/yyyy HH:mm '- UTC' K")            
-    $searchText = Read-Host ("$searchPrompt")
-    $getUsers = (Get-MgBetaUser | Where-Object {$_.$searchPropriety -like "*$searchText*"} | Select-Object DisplayName,Id) 
-    Write-Host ("`nStarting search: $DateTimeRun`n") -ForegroundColor Green
-    $found=$getUsers | Measure-Object
-    Write-Host "Number of matches found:" $found.Count
-    if (([string]::IsNullOrEmpty($getUsers))) {
-    Write-Host "No matching users found..." -ForegroundColor Red
-    tryAgain
-    } else {
-    foreach ($user in $getUsers) {
-        $userId = $user.Id
-        $userDN = $user.DisplayName
-        $OutFile = "$Path\$userDN - $userId.txt"
-        Write-Host "User:" $userDN
-        userInfo
-        }
-    }          
-    openOutPath    
-    tryAgain
-
+            $DateTimeRun = (Get-Date -Format "dddd MM/dd/yyyy HH:mm '- UTC' K")            
+            $searchText = Read-Host ("$searchPrompt")
+            $getUsers = (Get-MgBetaUser | Where-Object {$_.$searchPropriety -like "*$searchText*"} | Select-Object DisplayName,Id) 
+            Write-Host ("`nStarting search: $DateTimeRun`n") -ForegroundColor Green
+            $found=$getUsers | Measure-Object
+            Write-Host "Number of matches found:" $found.Count
+            if (([string]::IsNullOrEmpty($getUsers))) {
+            Write-Host "No matching users found...`n" -ForegroundColor Red
+            findBy
+            } else {
+            foreach ($user in $getUsers) {
+                $userId = $user.Id
+                $userDN = $user.DisplayName
+                $OutFile = "$Path\$userDN - $userId.txt"
+                Write-Host "User:" $userDN
+                userInfo
+                }
+            }              
+            findBy
 }
 
 
 function userInfo {    
-    $DateTimeRun = (Get-Date -Format "dddd MM/dd/yyyy HH:mm '- UTC' K")
-    checkTemp
-    Write-Output "$DateTimeRun" | Out-File $OutFile
-    (Get-MgBetaUser -UserId $userId).PSObject.Properties | Where-Object {$_.Value -notlike "Microsoft*" -and $_.Name -notlike "Security*"} | Format-Table Name,Value | Out-File $OutFile -Append
-    Write-Host "Output is saved in $OutFile`n" -ForegroundColor Green
-}
-
-function tryAgain {
-        $repeat = Read-Host ("Do you want to search again? Y/n")
-        if ($repeat -eq "y") {
-            findBy
-        }
-        Else {
-            endSession
-        }
+            $DateTimeRun = (Get-Date -Format "dddd MM/dd/yyyy HH:mm '- UTC' K")
+            checkTemp
+            Write-Output "$DateTimeRun" | Out-File $OutFile
+            (Get-MgBetaUser -UserId $userId).PSObject.Properties | Where-Object {$_.Value -notlike "Microsoft*" -and $_.Name -notlike "Security*"} | Format-Table Name,Value | Out-File $OutFile -Append
+            Write-Host "Output is saved in $OutFile`n" -ForegroundColor Green
 }
 
 function openOutPath {
-        $openOutPath = Read-Host ("Do you want to open the output folder? Y/n")
-            if ($openOutPath -eq "y") {
-                Write-Host "Trying to open $Path`n" -ForegroundColor Green
-                Start-Sleep 2
-                Start-Process $Path
-            }
-            Else {            
-                Write-Host "Will not open the output folder..." -ForegroundColor Green
-            }
+            Write-Host "Trying to open $Path`n" -ForegroundColor Green
+            Start-Sleep 2
+            Start-Process $Path           
+            Write-Host "Will now open the output folder...`n" -ForegroundColor Green
 }
 
 function endSession {            
-        Write-Host "`nTerminating Session" -ForegroundColor Green
-        Disconnect-MgGraph
-        Write-Host "Session Ended" -ForegroundColor Green
-        Exit
+            Write-Host "`nTerminating Session" -ForegroundColor Green
+            Disconnect-MgGraph
+            Write-Host "Session Ended" -ForegroundColor Green
+            Break
+            Exit
 }
 
 checkMicrosoftGraphUsersModule
